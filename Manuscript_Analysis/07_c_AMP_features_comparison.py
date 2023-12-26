feb4b16760fd697db051b684d77010109a11562d
Luis Pedro Coelho <luis@luispedro.org>
2023-12-26 10:17:25 +1000

    ENH Better figure
    Use partly overlapping density plots

--- Manuscript_Analysis/07_c_AMP_features_comparison.py
+++ Manuscript_Analysis/07_c_AMP_features_comparison.py
@@ -3,8 +3,10 @@
 # Here we will show how c_AMPs from AMPSphere are relatively similar to those in the training set of Macrel (Santos-Júnior et al., 2020) and DRAMP 3.0 (Shi et al., 2021). To that, we will use pre-computed features calculated using Macrel's internal scripts from the peptide sequences in those three databases.
 
 import pandas as pd
+import numpy as np
 import seaborn as sns
 from matplotlib import pyplot as plt
+from matplotlib import cm
 
 ampsphere = pd.read_csv('../data_folder/ampsphere_v2022-03.features.tsv.gz', sep="\t")
 dramp = pd.read_csv('../data_folder/dramp_v3.features.tsv.gz', sep="\t")
@@ -23,13 +25,16 @@ for feats in [ampsphere, dramp, macrel]:
            axis=1,
            inplace=True)
 
-def getminmax(feat, ampsphere, dramp, macrel):
-    a, b = [], []
-    for j in [ampsphere, dramp, macrel]:
-        a.append(j[feat].min())
-        b.append(j[feat].max())
-    return min(a), max(b)
-
+def getlims(feat):
+    feats = [
+            ampsphere[feat].values,
+            dramp[feat].values,
+            macrel[feat].values
+            ]
+    xs = np.concatenate(feats)
+    xs.sort()
+    cut = 10
+    return xs[cut], xs[-cut]
 
 graphkey = {
             'a': ['length',     'Length (residues)'],
@@ -43,19 +48,20 @@ graphkey = {
             'i': ['hmoment',    'Hydrophobic moment']
             }
 
-fig, axarr = plt.subplot_mosaic([
-                                 ['a', 'b', 'c'],
-                                 ['d', 'e', 'f'],
-                                 ['g', 'h', 'i'],
-                                 ])
-
-
-for k in graphkey:
+fig = plt.figure()
+for ix,k in enumerate(graphkey):
+    pos_i = ix % 3
+    pos_j = ix // 3
     feat, label = graphkey[k]
-    ax = axarr[k]
-    ax.clear()
-    for data,data_label in zip([ampsphere, dramp, macrel],
-                               ['AMPSphere', 'DRAMP', 'Macrel']):
+    ax0 = fig.add_axes([.025 + pos_i * .3, 0.075 + pos_j * .3, .25, .12])
+    ax1 = fig.add_axes([.025 + pos_i * .3, 0.1125 + pos_j * .3, .25, .12], sharex=ax0)
+    ax2 = fig.add_axes([.025 + pos_i * .3, 0.150 + pos_j * .3, .25, .12], sharex=ax0)
+    for data,data_label,ax,c in zip([ampsphere, dramp, macrel],
+                                ['AMPSphere', 'DRAMP', 'Macrel'],
+                                [ax0,ax1,ax2],
+                                cm.Dark2.colors,
+                                ):
+        ax.patch.set_visible(False)
         sns.kdeplot(ax=ax,
                     fill=True,
                     bw_method='silverman',
@@ -64,19 +70,22 @@ for k in graphkey:
                     data=data,
                     x=feat,
                     label=data_label,
+                    color=c,
                     )
-    ax.set_xlim(getminmax(feat,
-                            ampsphere,
-                            dramp,
-                            macrel))
-    if k == 'a':
-       ax.legend()
+        ax.set_xlim(getlims(feat))
+        ax.set_yticks([])
+        ax.set_ylabel(None)
+        ax.set_xlabel(None)
+        ax.spines['left'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        if k == 'a':
+           ax.legend()
+    for ax in [ax1, ax2]:
+        ax.spines['bottom'].set_visible(False)
+        ax.get_xaxis().set_visible(False)
 
-    ax.set_yticks([])
-    ax.set_ylabel(None)
-    ax.set_title(label,
+    ax2.set_title(label,
                  fontfamily='Sans Serif',
                  fontsize='large',
                  loc='left')
-
-fig.tight_layout()
