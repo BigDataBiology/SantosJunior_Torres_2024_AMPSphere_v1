c6118e5132ca34066cda6e359f95c81ce2b85909
Luis Pedro Coelho <luis@luispedro.org>
2023-12-28 11:34:47 +1000

    RFCT Remove use of older APIs

--- General_Scripts/01_resource_generation/utils/cluster_analysis.py
+++ General_Scripts/01_resource_generation/utils/cluster_analysis.py
@@ -1,6 +1,6 @@
 def analyze_level_III(analysis_folder):
     '''
-    Loads fasta file for AMPSphere and 
+    Loads fasta file for AMPSphere and
     create a dataframe with the sequence,
     access and family; also computes
     the families which will be selected by
@@ -10,11 +10,11 @@ def analyze_level_III(analysis_folder):
     import pandas as pd
     from collections import Counter
     from Bio import SeqIO
-        
+
     # loading info about peptides and families
     infile = f'{analysis_folder}/AMPSphere_v.2022-03.faa.gz'
     lv3 = dict()
-    for record in SeqIO.parse(gzip.open(infile, 
+    for record in SeqIO.parse(gzip.open(infile,
                                         'rt',
                                         encoding='utf-8'),
                               'fasta'):
@@ -43,7 +43,7 @@ def organize_house(analysis_folder):
     by this script
     '''
     import os
-    
+
     # creating folders for results
     os.makedirs(f'{analysis_folder}/families', exist_ok=True)
     subdirs = ['fastas',
@@ -52,7 +52,7 @@ def organize_house(analysis_folder):
                'tree_fig',
                'hmm',
                'hmm_logo']
-    
+
     for folder in subdirs:
         os.makedirs(f'{analysis_folder}/families/{folder}',
                     exist_ok=True)
@@ -62,12 +62,11 @@ def fasta_files(lv3, select_fam, analysis_folder):
     '''
     Generates the fasta files per family
     '''
-    import pandas as pd
-    
     # computing results per family
-    for f in select_fam:
-        df = lv3[lv3['family'] == f]
-        ofile = f'{analysis_folder}/families/fastas/{f}.faa'  
+    by_family = lv3.groupby('family').groups
+    for f, ixs in by_family.items():
+        df = lv3.loc[ixs]
+        ofile = f'{analysis_folder}/families/fastas/{f}.faa'
         with open(ofile, 'w') as db:
             for row in df.itertuples():
                 db.write(f'>{row.access}\n{row.sequence}\n')
@@ -78,24 +77,26 @@ def alignments(select_fam, analysis_folder):
     Computes alignments from
     the fasta files generated
     before
-    '''                
-    from Bio.Align.Applications import MuscleCommandline
-    
+    '''
+    import subprocess
     for f in select_fam:
+        f = 'SPHERE-III.000_002'
         ifile = f'{analysis_folder}/families/fastas/{f}.faa'
         ofile = f'{analysis_folder}/families/aln/{f}.aln'
-        muscle_cline = MuscleCommandline(input=ifile,
-                                         out=ofile,
-                                         diags=True,
-                                         maxiters=1)
-        muscle_cline()
-        
+
+        subprocess.check_call(
+                ' '.join(['muscle',
+                        '-align', ifile,
+                        '-output', ofile,
+                        '-maxiters', '1',
+                        '-diags',
+                        ])
 
 def trees(select_fam, analysis_folder):
     '''
     Computes trees and their ascII representation
     from the alignment files generated before
-    '''                
+    '''
     from .phylogen import treebuilder, draw_ascii
 
     for f in select_fam:
@@ -119,14 +120,14 @@ def hmm(select_fam, data_folder, analysis_folder):
     '''
     Computes HMM profile and logo
     from the alignment files generated before
-    '''                
-    from .hmm import hbuild
-    
+    '''
+    from utils.hmm import hbuild
+
     for f in select_fam:
         # computing HMM profile
         ofile = f'{analysis_folder}/families/hmm/{f}.hmm'
         i2file = f'{analysis_folder}/families/aln/{f}.aln'
-        
+
         hbuild(i2file,
                ofile,
                f)
@@ -138,7 +139,7 @@ def hmmlogo(select_fam, data_folder, analysis_folder):
     '''
     import lzma
     from .hmm import pict_hmmlogo
-    
+
     for f in select_fam:
         # computing HMM logo
         alph = lzma.open(f'{data_folder}/alph.json')
@@ -150,13 +151,13 @@ def hmmlogo(select_fam, data_folder, analysis_folder):
 
 def process_cluster():
     import os
-    
+
     data_folder = 'data/'
     analysis_folder = 'analysis/'
-    
+
     for folder in [data_folder, analysis_folder]:
         os.makedirs(folder, exist_ok=True)
-        
+
     print('Retrieve sequences and families')
     lv3, select_fam = analyze_level_III(analysis_folder)
     print('Generate folders')
